108396b4cb4868f3a7c749faaf1cffccc4c27e4e
Jeyaram <jram.pet@gmail.com>
2021-05-24 12:09:42 +0530

    latest commit

--- weatherAndNews/Views/CardView.swift
+++ weatherAndNews/Views/CardView.swift
@@ -14,7 +14,7 @@ class CardView: UIView {
 
     @IBInspectable var shadowOffsetWidth: Int = 0
     @IBInspectable var shadowOffsetHeight: Int = 3
-    @IBInspectable var shadowColor: UIColor? = UIColor.black
+    @IBInspectable var shadowColor: UIColor? = UIColor.lightGray
     @IBInspectable var shadowOpacity: Float = 0.5
 
     override func layoutSubviews() {
